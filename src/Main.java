be6b73d54a602947349577f87e79cb5a2d8a2ba5
Aiman Salam <108806913+Rudoasm@users.noreply.github.com>
2024-04-18 14:02:13 +0530

    starter code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,83 @@
-public class Main {
-    public static void main(String[] args) {
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    static char[][] map;
+    static Node startNode;
+    static Node finishtNode;
+
+    public static void main(String[] args) {
+        map = mapReader("Mazes/maze10_3.txt");
+        printMap(map);
     }
+
+    // Reading a map from an input text file----------------------------------------------------------------------------
+    public static char[][] mapReader(String filePath) {
+        char[][] map = null;
+        try {
+            BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
+
+            // Getting the dimensions of the map-------------------------------------
+            int rows = 0;
+            int cols = 0;
+            while (fileReader.readLine() != null) {
+                rows++;
+            }
+            fileReader.close();
+
+            fileReader = new BufferedReader(new FileReader(filePath));
+            String line = fileReader.readLine();
+            if (line != null) {
+                cols = line.length();
+            }
+            fileReader.close();
+
+            map = new char[rows][cols];
+            //-----------------------------------------------------------------------
+
+            // Writing values into the map-------------------------------------------
+            fileReader = new BufferedReader(new FileReader(filePath));
+            for (int y = 0; y < rows; y++) {
+                line = fileReader.readLine();
+                for (int x = 0; x < cols; x++) {
+                    char character = line.charAt(x);
+                    map[y][x] = character;
+
+                    // Getting the starting and finishing positions---
+                    if (character == 'S') {
+                        startNode = new Node(x+1, y+1);
+                    } else if (character == 'F') {
+                        finishtNode = new Node(x+1, y+1);
+                    }
+                    //------------------------------------------------
+                }
+            }
+            fileReader.close();
+            //-----------------------------------------------------------------------
+
+        } catch (FileNotFoundException e) {
+            System.out.println("The File you are looking for does not exist.");
+        } catch (IOException e) {
+            System.out.println("There was an error while reading from the File.");
+        }
+
+        return map;
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Printing a map represented using a 2D array----------------------------------------------------------------------
+    public static void printMap(char[][] map) {
+        for (char[] charRow : map) {
+            for (char character : charRow) {
+                System.out.print(character);
+            }
+            System.out.println();
+        }
+    }
+    //------------------------------------------------------------------------------------------------------------------
 }
---
+++ src/Node.java
@@ -0,0 +1,63 @@
+public class Node {
+    private Node prevNode;
+    private Node nextNode;
+    private int xPosition;
+    private int yPosition;
+    private int distance;
+
+    public Node(int x, int y) {
+        this.prevNode = null;
+        this.nextNode = null;
+        this.xPosition = x;
+        this.yPosition = y;
+        this.distance = -1;
+    }
+
+    public Node(Node prevNode, Node nextNode, int x, int y, int distance) {
+        this.prevNode = prevNode;
+        this.nextNode = nextNode;
+        this.xPosition = x;
+        this.yPosition = y;
+        this.distance = distance;
+    }
+
+    // Getters----------------------------------------------------------------------------------------------------------
+    public Node getPrev() {
+        return prevNode;
+    }
+    public Node getNext() {
+        return nextNode;
+    }
+    public int getX() {
+        return xPosition;
+    }
+    public int getY() {
+        return yPosition;
+    }
+    public int getDistance() {
+        return distance;
+    }
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Setters----------------------------------------------------------------------------------------------------------
+    public void setPrev(Node prevNode) {
+        this.prevNode = prevNode;
+    }
+
+    public void setNext(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public void setX(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public void setY(int yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+    //------------------------------------------------------------------------------------------------------------------
+}
